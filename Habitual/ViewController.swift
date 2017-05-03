6f663bb774e4338f731bc03d2851dcb0deed17d2
Peter Fitz <peterfitz@danielles-macbook-pro-2.local>
2017-05-03 13:08:52 -0400

    Initial Commit

---
+++ Habitual/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Habitual
+//
+//  Created by Peter Fitz on 5/3/17.
+//  Copyright © 2017 Peter Fitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
